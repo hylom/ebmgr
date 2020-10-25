3e7f81974cd72c4a09356863085e81c098d2cea5
Hiromichi Matsushima <hylom@hylom.net>
2020-10-25 22:01:24 +0900

    react-app: add Sidebar component

---
+++ react-app/src/Sidebar.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import getClient from './client';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+  constructor () {
+    super();
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+        <div className="Sidebar">
+        </div>
+    );
+  }
+}
+
+export default Sidebar;
