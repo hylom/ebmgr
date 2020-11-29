75901b456572796576effaee47929e3740b2b56f
Hiromichi Matsushima <hylom@hylom.net>
2020-11-29 23:40:12 +0900

    react-app: fix directory-list design

--- react-app/plugins/directory-list/Directories.js
+++ react-app/plugins/directory-list/Directories.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
+import openIcon from './icons/folder-remove.svg';
+import closedIcon from './icons/folder-add.svg';
+import folderIcon from './icons/folder.svg';
+import './Directories.css';
 
 class Directories extends Component {
   constructor () {
     super();
     this.state = {
       directoryList: {},
+      directoryState: {},
     };
     this.filterByDir = this.filterByDir.bind(this);
+    this.openDirectory = this.openDirectory.bind(this);
   }
 
   componentDidMount() {
@@ -20,20 +26,49 @@ class Directories extends Component {
     window.parent.bookmanager.execQuery(`vpath ^= "${trace}"`);
   }
 
+  openDirectory(vpath, opened) {
+    this.setState(state => {
+      state.directoryState[vpath] = state.directoryState[vpath] || {};
+      state.directoryState[vpath].opened = opened;
+      return {directoryState: state.directoryState};
+    });
+  }
+
   _renderDirectory(dirList, trace) {
     trace = trace || "";
     const nodes = [];
     for (const name in dirList) {
       const currentTrace = trace ? `${trace}/${name}` : name;
-      if (dirList[name].length == 0) {
+
+      if (Object.keys(dirList[name]).length == 0) {
         nodes.push(<li key={currentTrace}>
+                     <img className="dir-icon"
+                     src={folderIcon}
+                     alt="folder" />
                    <span onClick={() => {this.filterByDir(currentTrace);}}>{name}</span>
                    </li>);
       } else {
-        const subDir = this._renderDirectory(dirList[name], currentTrace);
-        nodes.push(<li key={name}>
-                   <span onClick={() => {this.filterByDir(currentTrace);}}>{name}</span>
-                   {subDir}</li>);
+        const dirState = this.state.directoryState[currentTrace] || {};
+        if (dirState.opened) {
+          const subDir = this._renderDirectory(dirList[name], currentTrace);
+          nodes.push(<li key={name} className="open">
+                     <img className="dir-icon"
+                     src={openIcon}
+                     alt="open"
+                     title="clock to close"
+                     onClick={() => {this.openDirectory(currentTrace, false);}} />
+                     <span onClick={() => {this.filterByDir(currentTrace);}}>{name}</span>
+                     {subDir}</li>);
+        } else {
+          nodes.push(<li key={name} className="closed">
+                     <img className="dir-icon"
+                     src={closedIcon}
+                     alt="closed"
+                     title="clock to open"
+                     onClick={() => {this.openDirectory(currentTrace, true);}} />
+                     <span onClick={() => {this.filterByDir(currentTrace);}}>{name}</span>
+                     </li>);
+        }
       }
     }
     return <ul>{nodes}</ul>;
